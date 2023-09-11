395b2dc784ba75ad1b1b08d96402629cb01495f2
William Chávez Díaz <chavezdiaz4@gmail.com>
2023-09-11 11:17:06 -0500

    correcciones menores

--- tests/CounterApp.test.jsx
+++ tests/CounterApp.test.jsx
@@ -6,14 +6,16 @@ describe('Pruebas de Snapshot en <CounterApp />', () => {
 
     test('Debe hacer match con el snapshot', () => {
         const valorFalla = 100;
-        const { container } = render(<CounterApp value={initialValue} />);
+        const { container } = render(<CounterApp value={valorFalla} />);
         expect(container).toMatchSnapshot();
     });
 
+    // Genera advertencia
     test('Debe hacer match con el snapshot con valor sin definir', () => {
+        const notValue = 'No se envio/definio el value';
         const { container, getByText } = render(<CounterApp />);
-        const h2Element = getByText('No se envió/definió el value');
-        expect(h2Element).toBeInTheDocument();
+        const h2Element = getByText(notValue);
+        expect(h2Element.textContent).toBe(notValue);
         expect(container).toMatchSnapshot();
     });
 
